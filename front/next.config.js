dfdae9deec2d4ac880bbe4878c16210fbb9c9ab2
Dylan Burris <dylan@illuminedigital.com>
2023-03-25 20:53:58 -0700

    =Config adjustment.s

--- front/next.config.js
+++ front/next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  assetPrefix: process.env.NODE_ENV === 'production' ? `${process.env.ASSET_BUCKET}/nextjs` : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? `${process.env.ASSET_ADDRESS}/nextjs` : '',
   images: {
     domains: ['media-amazon.com'],
   },
